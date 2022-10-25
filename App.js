e2a69ba0ff1425fec52fee6ebab780c730086a90
sonsieg <maxseo9x@gmail.com>
2022-10-25 20:46:15 +0700

    25/02 login google working

--- App.js
+++ App.js
@@ -9,16 +9,11 @@ import { ActivityIndicator } from 'react-native';
 import { Colors } from './src/assets';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
-GoogleSignin.configure();
 const App = () => {
   GoogleSignin.configure({
-    scopes: [
+    scopes: ['https://www.googleapis.com/auth/userinfo.profile'], //scopes as you need
+    webClientId:
       '577290104875-bsve3e8pgqluggvp0jot70muqbvup7ba.apps.googleusercontent.com',
-    ], // what API you want to access on behalf of the user, default is email and profile
-    webClientId: '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', // client ID of type WEB for your server (needed to verify user ID and offline access)
-    offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
-    forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
-    // iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
   });
   console.reportErrorsAsExceptions = false;
   return (
